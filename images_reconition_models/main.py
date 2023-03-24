b7804212e255a32fb74b3ac9a50d07bb72bee30d
MrLakige <82679564+MrLakige@users.noreply.github.com>
2023-03-24 15:16:58 +0100

    Add files via upload

---
+++ images_reconition_models/main.py
@@ -0,0 +1,14 @@
+from imageai.Detection import ObjectDetection
+import os
+
+execution_path = os.getcwd()
+
+detector = ObjectDetection()
+detector.setModelTypeAsYOLOv3()
+detector.setModelPath( os.path.join(execution_path , "yolov3.pt"))
+detector.loadModel()
+detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "image2.jpg"), output_image_path=os.path.join(execution_path , "image3new.jpg"), minimum_percentage_probability=30)
+
+for eachObject in detections:
+    print(eachObject["name"] , " : ", eachObject["percentage_probability"], " : ", eachObject["box_points"] )
+    print("--------------------------------")
