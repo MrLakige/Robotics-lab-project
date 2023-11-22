599ea2875c104e2955990a38471e07f0b543f22f
Michele Callegari <michele.callegari@studenti.unitn.it>
2023-11-22 09:41:01 +0100

    Update inverseKinematics.h

--- kinematics/include/inverseKinematics.h
+++ kinematics/include/inverseKinematics.h
@@ -4,6 +4,6 @@
 #include <Eigen/Core>
 #include <Eigen/Geometry>
 
-Eigen::Matrix<double, 6, 8> ur5inverseKinematics(Eigen::Vector3d p60, Eigen::Matrix3d R60);
+Eigen::Matrix<double, 8, 6> ur5inverseKinematics(Eigen::Vector3d p60, Eigen::Matrix3d R60);
 
 #endif
