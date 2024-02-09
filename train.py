0f636aba1fc40c597164083b80026011ac1d89cb
MrLakige <callegari.michele.mc@gmail.com>
2024-02-09 09:54:49 +0100

    Remove unsued files

--- train.py
+++
@@ -1,7 +0,0 @@
-from imageai.Detection.Custom import DetectionModelTrainer
-
-trainer = DetectionModelTrainer()
-trainer.setModelTypeAsYOLOv3()
-trainer.setDataDirectory(data_directory="blocks")
-trainer.setTrainConfig(object_names_array=["blocks"], batch_size=5, num_experiments=100, train_from_pretrained_model="yolov3.pt")
-trainer.trainModel()
